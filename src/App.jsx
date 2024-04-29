2beff56c3e49a2dd5f555ef78681f3777ad89245
Anand patel <panand054@gmail.com>
2024-04-29 16:45:47 +0530

    data show

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import Routes from "./Routes/RoutesCmponent";
 import Nav from "../src/components/Nav";
 import axios from "axios";
+import { useState } from "react";
 
 const App = () => {
+  const [user, setuser] = useState([]);
   const getUsers = async () => {
     try {
-      const { data } = await axios.get("https://jsonplaceholder.typicode.com/users");
+      const { data } = await axios.get(
+        "https://jsonplaceholder.typicode.com/users"
+      );
       console.log(data);
+      setuser(data);
     } catch (error) {
       console.log(error);
     }
@@ -15,7 +20,16 @@ const App = () => {
   return (
     <div className="w-[80%] m-auto h-screen">
       <Nav />
-      <button className="bg-green-500 py-2 px-5 mt-3" onClick={getUsers}>Get Users</button>
+      <button className="bg-green-500 py-2 px-5 mt-3" onClick={getUsers}>
+        Get Users
+      </button>
+      <div>
+        {user.map((user, i) => (
+          <h2 key={i}>
+            {user.id}. {user.name}
+          </h2>
+        ))}
+      </div>
       <Routes />
     </div>
   );
